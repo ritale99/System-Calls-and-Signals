b4ea494f3f8354c0ff3f7668f8685f432a20cb86
MDotSan <71162128+MDotSan@users.noreply.github.com>
2020-09-12 16:14:08 -0400

    Update TimeSysCall.c
    Added 2 struct for time
    for loop counter i 
    loop count set to 100,000
    final time, and avg_time

--- TimeSysCall.c
+++ TimeSysCall.c
@@ -1,17 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 // Place any necessary global variables here
 
 int main(int argc, char *argv[]){
-
-	/*
-	 *
-	 * Implement Here
-	 *
-	 *
-	 */
-
+	struct timeval tv_start;
+	struct timeval tv_end;
+	int i = 0;
+	int loopCount = 100000;
+	double final_time = 0;
+	double avg_time = 0;
+	gettimeofday(&tv_start, NULL);
+	for ( i = 0 ; i < loopCount ; i++ ){
+		getpid();
+	}
+	gettimeofday(&tv_end, NULL);
+	final_time = tv_end.tv_usec - tv_start.tv_usec;
+	avg_time = (final_time)/loopCount;
+	printf("Syscalls Performed: %d\n", loopCount);
+	printf("Total Elapse Time: %f microseconds\n", final_time);
+	printf("Average Time Per Syscall: %f microseconds\n", avg_time);
 	return 0;
-
 }
