ea32e1ba2b40573973dcf65f36eef8bd0cfe1150
ritale99 <rra76@scarletmail.rutgers.edu>
2020-09-14 13:58:28 -0400

    added header and wait args

--- TimeFork.c
+++ TimeFork.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/time.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]){ 
 	struct timeval tv_start;
 	struct timeval tv_end;
 	int i = 0;
@@ -13,12 +15,15 @@ int main(int argc, char *argv[]){
 	double final_time = 0;
 	double avg_time = 0;
 	gettimeofday(&tv_start, NULL);
+
 	for (i = 0 ; i < loopCount ; i++ ){
 		if ( fork() == 0 )//child
 			exit(0);
 		else			  //parent
-			wait();
+			wait(NULL);
 	}
+	
+
 	gettimeofday(&tv_end, NULL);
 	final_time = tv_end.tv_usec - tv_start.tv_usec;
 	avg_time = final_time / loopCount;
